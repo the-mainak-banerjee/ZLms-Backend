3e907a731e18c5abc431095c16b9afeaa146eddf
Mainak <developer.mainakbanerjee@gmail.com>
2024-07-23 18:49:15 +0530

    removed unused codes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-// Always wrap DB in try catch
-// DB is always in another continent, so always use async await while talking to DB
-
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({
   path: "./env",
 });
-
-console.log(process.env.PORT);
 
 connectDB();
